caae0404770e93eec0ed9ea6ba995eb9c45f3887
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2025-01-27 19:55:59 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите путь к файлу:");
+            string filePath = Console.ReadLine()?.Trim('"');
+            string searchWord = "bicycle";
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Файл не найден.");
+                return;
+            }
+
+            ProcessStartInfo notepadStartInfo = new ProcessStartInfo
+            {
+                FileName = @"C:\\Windows\\System32\\notepad.exe",
+                Arguments = filePath,
+                UseShellExecute = true
+            };
+
+            try
+            {
+                Process notepadProcess = Process.Start(notepadStartInfo);
+                Console.WriteLine("Файл открыт.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Не удалось открыть: {ex.Message}");
+                return;
+            }
+
+            try
+            {
+                string fileContent = File.ReadAllText(filePath);
+                int wordCount = fileContent.Split(new[] { ' ',','}).Count(word => string.Equals(word, searchWord));
+
+                Console.WriteLine($"Слово '{searchWord}' встречается {wordCount} раз(а).");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+} 
